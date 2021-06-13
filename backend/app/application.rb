9401ff8902e6377e2d8ccc447d78aac8923752fb
Xiaochen Xu <xiaochex@gmail.com>
2021-06-13 16:41:36 -0400

    backend build

--- backend/app/application.rb
+++ backend/app/application.rb
@@ -4,9 +4,37 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match(/test/) 
-      return [200, { 'Content-Type' => 'application/json' }, [ {:message => "test response!"}.to_json ]]
+      
+    if req.path.match(/gifts/) 
+      
+      if req.env["REQUEST_METHOD"] == "POST"
+        input = JSON.parse(req.body.read)
+        user_id = req.path.split("/users/").last.split('/gifts').first
+        user = User.find_by(id: user_id)
+        gift = user.gifts.create(name: input["name"])
+        return [200, { 'Content-Type' => 'application/json' }, [ gift.to_json ]]
+      elsif req.env["REQUEST_METHOD"] == "DELETE"
+        user_id = req.path.split("/users/").last.split('/gifts/').first
+        user = User.find_by(id: user_id)
+        gift_id = req.path.split('/gifts/').last
+        user_gift = user.gifts.find_by(id: gift_id)
+        user_gift.destroy()
+      end
 
+    elsif req.path.match(/users/)
+      if req.env["REQUEST_METHOD"] == "POST"
+        input = JSON.parse(req.body.read)
+        user = User.create(name: input["name"])
+        return [200, { 'Content-Type' => 'application/json' }, [ user.to_json ]]
+      else
+        if req.path.split("/users").length == 0 
+          return [200, { 'Content-Type' => 'application/json' }, [ User.all.to_json ]]
+        else
+          user_id = req.path.split("/users/").last
+          return [200, { 'Content-Type' => 'application/json' }, [ User.find_by(id: user_id).to_json({:include => :gifts})]]
+        end
+      end
+    
     else
       resp.write "Path Not Found"
         
--- backend/db/migrate/20210613191158_create_gifts.rb
+++ backend/db/migrate/20210613191158_create_gifts.rb
@@ -1,4 +1,8 @@
 class CreateGifts < ActiveRecord::Migration[5.2]
   def change
+    create_table :gifts do |t|
+      t.string :name
+      t.integer :user_id
+    end
   end
 end
